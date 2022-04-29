ccd6c9b2b15f974390d545513432839c50ee4f41
Erkinov Azim <erkinovazim2705@gmail.com>
2022-04-29 17:33:16 +0500

    created CompanyController and tested GetAllCompanies() method

---
+++ Controllers/CompanyEmployees.cs
@@ -0,0 +1,34 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace WebApiMS.Controllers
+{
+    [Route("api/companies")]
+    [ApiController]
+    public class CompanyEmployees : ControllerBase
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly ILoggerManager _logger;
+        public CompanyEmployees(IRepositoryManager repository, ILoggerManager logger)
+        {
+            _repository= repository;   
+            _logger= logger;
+        }
+        [HttpGet]
+        public IActionResult GetCompanies()
+        {
+            try
+            {
+                var companies = _repository.Company.GetAllCompanies(trackChanges: false);
+                return Ok(companies);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"Something went wrong in the {nameof(GetCompanies)} action {ex}");
+                return StatusCode(500, "Internal server error!");
+            }
+        }
+    }
+}
